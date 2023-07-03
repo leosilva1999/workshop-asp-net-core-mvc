e397a5ba6638dbe87efd3334ca4e44cf1f02b2c5
Leonardo <leopereirasilva86@gmail.com>
2023-07-03 17:24:48 -0300

    Returning custom error page

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,7 +3,10 @@ using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Services;
 using SalesWebMvc.Services.Exceptions;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
 
 namespace SalesWebMvc.Controllers
 {
@@ -43,12 +46,12 @@ namespace SalesWebMvc.Controllers
 		{
 			if (id == null)
 			{
-				return NotFound();
+				return RedirectToAction(nameof(Error), new {message = "Id not provided"});
 			}
 			var obj = _sellerService.FindById(id.Value);
 			if (obj == null)
 			{
-				return NotFound();
+				return RedirectToAction(nameof(Error), new { message = "Id not found" });
 			}
 
 			return View(obj);
@@ -66,12 +69,12 @@ namespace SalesWebMvc.Controllers
 		{
 			if (id == null)
 			{
-				return NotFound();
+				return RedirectToAction(nameof(Error), new { message = "Id not provided" });
 			}
 			var obj = _sellerService.FindById(id.Value);
 			if (obj == null)
 			{
-				return NotFound();
+				return RedirectToAction(nameof(Error), new { message = "Id not found" });
 			}
 
 			return View(obj);
@@ -81,13 +84,13 @@ namespace SalesWebMvc.Controllers
 		{
 			if (id == null)
 			{
-				return NotFound();
+				return RedirectToAction(nameof(Error), new { message = "Id not provided" });
 			}
 
 			var obj = _sellerService.FindById(id.Value);
 			if (obj == null)
 			{
-				return NotFound();
+				return RedirectToAction(nameof(Error), new { message = "Id not found" });
 			}
 
 			List<Department> departments = _departmentService.FindAll();
@@ -101,21 +104,27 @@ namespace SalesWebMvc.Controllers
 		{
 			if(id != seller.Id)
 			{
-				return BadRequest();
+				return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
 			}
 			try
 			{
 				_sellerService.Update(seller);
 				return RedirectToAction(nameof(Index));
 			}
-			catch(NotFoundException)
+			catch(ApplicationException e)
 			{
-				return NotFound();
+				return RedirectToAction(nameof(Error), new { message = e.Message });
 			}
-			catch (DbConcurrencyException)
+		}
+
+		public IActionResult Error(string message)
+		{
+			var viewModel = new ErrorViewModel
 			{
-				return BadRequest();
-			}
+				Message = message,
+				RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+			};
+			return View(viewModel);
 		}
 	}
 }
