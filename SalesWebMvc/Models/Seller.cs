c6ebcde0690bdc8ea4192de0fe6daef5bbf02d01
Leonardo <leopereirasilva86@gmail.com>
2023-07-04 12:31:26 -0300

    Validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -8,15 +8,24 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} Required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} an {1}")]
         public string Name { get; set; }
         [DataType(DataType.EmailAddress)]
-        public string Email { get; set; }
 
-        [Display(Name = "Birth Date")]
+        [EmailAddress(ErrorMessage = "Enter a valid e-mail")]
+		[Required(ErrorMessage = "{0} Required")]
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "{0} Required")]
+		[Display(Name = "Birth Date")]
 		[DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
 		public DateTime BirthDate { get; set; }
 
+		[Required(ErrorMessage = "{0} Required")]
+		[Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
 		[Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
 		public double BaseSalary { get; set; }
